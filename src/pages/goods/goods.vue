4de76e72ca53eee84f9c8b35c7479ba6e2483712
KyleYin <131144602+KyleWinter@users.noreply.github.com>
2023-11-10 17:01:01 +0800

    Update goods.vue

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -63,11 +63,38 @@
       </nut-space>
     </nut-cell>
     <nut-cell>
-      <div class="confirmbut1">
-        <nut-button type="warning" @click="buyVisible = true"
-          >兑换商品</nut-button
-        >
-      </div>
+      <nut-row :gutter="5">
+        <nut-col :span="8">
+          <div
+            :style="{ fontSize: '14px', marginLeft: '10px', color: '#ffd777' }"
+          >
+            <a style="color: orange">可用积分：</a>
+          </div>
+          <nut-countup
+            :init-num="0.0"
+            :end-num="state.points"
+            :speed="5"
+            :during="1"
+            :to-fixed="2"
+            :color="orange"
+          ></nut-countup>
+          
+        </nut-col>
+        <nut-col :span="8"> 
+          <nut-button type="warning" @click="tipsClick">积分规则</nut-button>
+        </nut-col>
+        <nut-col :span="8">
+          <div class="confirmbut1">
+            <nut-button
+              type="warning"
+              @click="buyVisible = true"
+              v-model:disabled="state.but1"
+              v-model:loading="state.loading2"
+              >兑换</nut-button
+            >
+          </div>
+        </nut-col>
+      </nut-row>
     </nut-cell>
 
     <nut-popup
@@ -110,6 +137,19 @@
     </nut-popup>
   </view>
 
+  <nut-dialog
+    no-cancel-btn
+    title="积分规则"
+    :content="`<div style='text-align: left;'>
+      1. 积分获取：完成任务可以获得一定的积分
+      2. 兑换商品范围：积分可以用于兑换有库存的商品。
+      3. 兑换流程：浏览商城，选择您想要的商品，并确认兑换。兑换成功后，积分将被扣除，工作人员会通过电话联系您。
+      4. 积分结算：积分结算将每日进行一次，您获得的积分将会及时添加到您的账户中.
+      5. 积分规则限制：积分不可转让给其他用户.
+    </div>`"
+    v-model:visible="state.ruleVisble"
+  />
+
 </template>
   <script setup>
 import { ref, reactive, toRefs } from "vue";
@@ -130,20 +170,24 @@ const state = reactive({
   points: 50,
   goodId: routerParams?.id,
   child: {
-    u_id: "26adeeee-7994-11ee-b962-0242ac120002",
+    u_id: "20011",
   },
+  but1: false,
+  loading2: true,
+  ruleVisble:false,
 });
 
 // state.child.u_id = JSON.parse(wx.getStorageSync("child")).u_id
 
 async function getPoints() {
   await childrenApi.getChildrenPoints(state.child).then((res) => {
-    console.log("points",res.data);
+    console.log("points", res.data);
     state.points = res.data;
   });
 }
 
 async function getGoods() {
+  state.loading2 = true;
   const body = {
     goods_id: state.goodId,
   };
@@ -152,6 +196,11 @@ async function getGoods() {
     // console.log(res.data);
     state.goods = res.data;
   });
+  if (state.goods.goodsNum <= 0) {
+    notifyState.methods.cellClick("warning", "商品库存不足。", 0);
+    state.but1 = true;
+  }
+  state.loading2 = false;
 }
 
 const showFn = () => {
@@ -170,21 +219,17 @@ const notifyState = {
     show: false,
     desc: "",
     type: "primary",
+    duration: 5000,
   }),
   methods: {
-    cellClick(type, desc) {
+    cellClick(type, desc, duration) {
       notifyState.state.show = true;
       notifyState.state.type = type;
       notifyState.state.desc = desc;
+      notifyState.state.duration = duration;
     },
   },
 };
-
-async function buyGoods(body) {
-  const res = await mallApi.buyGoods(body);
-  return res
-  // console.log("points:",res.data);
-}
 
 console.log(Taro.getStorageSync("goods"));
 // console.log(JSON.parse(Taro.getStorageSync("goods"))
@@ -200,41 +245,62 @@ const change = (index) => {
 };
 
 const onCancel = () => {
-  notifyState.methods.cellClick("warning", "您已取消兑换。");
   console.log("event cancel");
   visible1.value = false;
   buyVisible.value = false;
 };
 
-const onOk = () => {
-  console.log("click ok");
+const tipsClick = () => {
+  state.ruleVisble = true;
+};
+
+async function onOk() {
+  state.loading2 = true;
   visible1.value = false;
   buyVisible.value = false;
+  notifyState.methods.cellClick(
+    "primary",
+    "正在操作中，请耐心等待，不要重复操作。",
+    5000
+  );
+  console.log("click ok");
+
   // console.log(JSON.parse(wx.getStorageSync("goods")).goodsId);
   // console.log(JSON.parse(wx.getStorageSync("child")).uId);
   const body = {
     u_id: state.child.u_id,
     goods_id: state.goodId,
   };
-  const mess = buyGoods(body);
-  console.log(mess)
+  var mess = 0;
+  await mallApi.buyGoods(body).then((res) => {
+    console.log(res.data);
+    mess = res.data;
+  });
+  console.log(mess);
+  console.log(mess == "666");
+  console.log(mess == 666);
   // console.log(mess.data)
-  if (-1 == mess.data) {
+  if (-1 == mess) {
     console.log("积分不足");
-    notifyState.methods.cellClick("danger", "您的积分不足。");
-  } else if (-2 == mess.data) {
+    var string =
+      "您的积分不足，当前积分为： " +
+      state.points +
+      "。商品所需积分为： " +
+      state.goods.goodsPoints;
+    notifyState.methods.cellClick("danger", string, 5000);
+  } else if (-2 == mess) {
     console.log("商品缺货");
-    notifyState.methods.cellClick("danger", "商品缺货中。");
-  } else if (666 == mess.data) {
+    notifyState.methods.cellClick("danger", "商品缺货中。", 5000);
+  } else if (666 == mess) {
     console.log("兑换成功");
-    notifyState.methods.cellClick("success", "您已成功兑换！");
+    notifyState.methods.cellClick("success", "您已成功兑换！", 5000);
   } else {
     console.log("兑换异常");
-    notifyState.methods.cellClick("danger", "兑换失败。");
+    notifyState.methods.cellClick("danger", "兑换失败,请联系客服。", 5000);
   }
   getGoods();
   getPoints();
-};
+}
 
 // 底部操作按钮触发
 const clickBtnOperate = () => {
@@ -347,12 +413,5 @@ const close = () => {};
     left: 50%;
     transform: translate(-50%, -50%);
   }
-}
-
-.confirmbut1 {
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 }
 </style>
