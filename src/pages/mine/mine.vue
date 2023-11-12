e4dcee6f720e6f15786b7f463d6eddf8ba042b22
Bill Luo <luohaonian@gmail.com>
2023-11-12 21:28:13 +0800

    avatar fix

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -6,7 +6,7 @@
       <nut-space :gutter="12"  center style="margin-top:15%">
         <nut-avatar size="large" >
           <img style="border-radius : 100%"
-            :src= "childAvatar.data" />
+            :src= "childAvatar" />
         </nut-avatar>
         <nut-space direction="vertical" fill>
             <nut-row style="font-size:18px;margin-top: 7px;">{{child.cName}}</nut-row>
@@ -94,7 +94,7 @@ console.log("99999999999999999999999999")
 console.log(child);
 
 
-//const childAvatar =  ref(0);
+const childAvatar =  ref(null);
 //async function getAvatar(){
 //  childAvatar.value = await childrenApi.getChildrenAvatar(tempChild);
 //  return childAvatar;
@@ -105,8 +105,8 @@ console.log(child);
 // const childAvatar = "https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png" ;
 
 // const childAvatar = await childrenApi.getChildrenAvatar(child);
-const childAvatar = tempChild.u_headpage_url;
-
+childAvatar.value = tempChild.u_headpage_url;
+console.log(childAvatar);
 
 //TODO
 const noticebarMessage = "在云南省保山市昌宁县，东北大学软件学院的同学们进行了“明光筑梦”计划“儿童之家”启用后的首次授课。授课内容涵盖理想信念教育、普通话推广、科学知识普及、未成年人保护等。同学们还走进大山，开展志愿服务帮扶活动。";
